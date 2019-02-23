05a048e7a71433c5943580880a53942d75c9d759
neelkanjariya1996 <neelkanjariya1996@gmail.com>
2019-02-22 19:25:10 -0500

    variable name changes

--- simulator.c
+++ simulator.c
@@ -44,182 +44,206 @@ typedef struct reb_t_ {
 	int val;
 } reb_t;
 
-int
-read_datamemory_file(int M[], int number_of_memory_locations) {
+void
+print_rgf (int R[]) {
+	
+	int i = 0;
 
-        FILE *read_datamemory;
+	printf("RGF:");
+	for (i = 0; i < MAX_REGISTERS - 1; i++) {
+		printf("<R%d,%d>,", i, R[i]);
+	}
+	printf("<R7,%d>", R[7]);
+	printf("\n");
+
+}
+
+void
+print_dam (int M[]) {
+	
+	int i = 0;
+
+	printf("DAM:");
+	for (i = 0; i < MAX_DATAMEMORY - 1; i++) {
+		printf("<%d,%d>,", i, M[i]);
+	}
+	printf("<7,%d>", M[i]);
+	printf("\n");
+
+}
+
+int
+read_dam(int M[]) {
+
+        FILE *read_dam;
         char str[6];
         char *pch;
-        int memory_index = 0;
-        int memory_value = 0;
+        int mem_ind = 0;
+        int mem_val = 0;
         int ret = 0;
         int i = 0;
 
-        read_datamemory = fopen("datamemory.txt", "r");
-        if (read_datamemory == NULL) {
+        read_dam = fopen("datamemory.txt", "r");
+        if (read_dam == NULL) {
                 fprintf(stderr, "Error reading file\n");
                 return 1;
         }
 
-        while (ret = fscanf(read_datamemory, "%s", str)) {
+        while (ret = fscanf(read_dam, "%s", str)) {
 
                 if (ret == EOF) {
                         break;
                 }
                 
 		pch = strtok(str, "<,>\n");
-                memory_index = atoi(pch);
-                for (i = 0; i < number_of_memory_locations; i++) {
-                        if (memory_index == i) {
+                mem_ind = atoi(pch);
+                for (i = 0; i < MAX_DATAMEMORY; i++) {
+                        if (mem_ind == i) {
                                 if (pch != NULL) {
                                         pch = strtok (NULL, "<,>\n");
-                                        memory_value = atoi(pch);
-                                        M[i] = memory_value;
+                                        mem_val = atoi(pch);
+                                        M[i] = mem_val;
                                 }
                         }
                 }
 
         }
 
-        fclose(read_datamemory);
+        fclose(read_dam);
 
         return 0;
 }
 
 int
-update_datamemory_file(int M[], int number_of_memory_locations) {
+update_dam(int M[]) {
 
-        FILE *read_datamemory;
+        FILE *read_dam;
         int i = 0;
 
-        read_datamemory = fopen("datamemory.txt", "w");
-        if (read_datamemory == NULL) {
+        read_dam = fopen("datamemory.txt", "w");
+        if (read_dam == NULL) {
                 fprintf(stderr, "Error reading file\n");
                 return 1;
         }
 
-        for (i = 0; i < number_of_memory_locations; i++) {
-                fprintf(read_datamemory, "<%d,%d>\n", i, M[i]);
+        for (i = 0; i < MAX_DATAMEMORY; i++) {
+                fprintf(read_dam, "<%d,%d>\n", i, M[i]);
         }
 	
-        fclose(read_datamemory);
+        fclose(read_dam);
 
         return 0;
 }
 
 int 
-read_register_file(int R[], int number_of_registers) {
+read_reg(int R[]) {
 
-	FILE *read_registers;
+	FILE *read_reg;
         char str[8];
         char *pch;
-        int register_index = 0;
-        int register_value = 0;
+        int reg_ind = 0;
+        int reg_val = 0;
         int ret = 0;
         int i = 0;
 
-        read_registers = fopen("registers.txt", "r");
-        if (read_registers == NULL) {
+        read_reg = fopen("registers.txt", "r");
+        if (read_reg == NULL) {
                 fprintf(stderr, "Error reading file\n");
                 return 1;
         }
 
-        while (ret = fscanf(read_registers, "%s", str)) {
+        while (ret = fscanf(read_reg, "%s", str)) {
 
                 if (ret == EOF) {
                         break;
                 }
 
                 pch = strtok(str, "<R,>\n");
-                register_index = atoi(pch);
-                for (i = 0; i < number_of_registers; i++) {
-                        if (register_index == i) {
+                reg_ind = atoi(pch);
+                for (i = 0; i < MAX_REGISTERS; i++) {
+                        if (reg_ind == i) {
                                 if (pch != NULL) {
                                         pch = strtok (NULL, "<R,>\n");
-                                        register_value = atoi(pch);
-                                        R[i] = register_value;
+                                        reg_val = atoi(pch);
+                                        R[i] = reg_val;
                                 }
                         }
                 }
 
         }
 
-        fclose(read_registers);
+        fclose(read_reg);
 
 	return 0;
 }
 
 int
-update_register_file(int R[], int number_of_registers) {
+update_reg(int R[]) {
 
-	FILE *read_registers;
+	FILE *read_reg;
 	int i = 0;
 	
-	read_registers = fopen("registers.txt", "w");
-        if (read_registers == NULL) {
+	read_reg = fopen("registers.txt", "w");
+        if (read_reg == NULL) {
                 fprintf(stderr, "Error reading file\n");
                 return 1;
         }
 
-        for (i = 0; i < number_of_registers; i++) {
-                fprintf(read_registers, "<R%d,%d>\n", i, R[i]);
+        for (i = 0; i < MAX_REGISTERS; i++) {
+                fprintf(read_reg, "<R%d,%d>\n", i, R[i]);
         }
 
-        fclose(read_registers);
+        fclose(read_reg);
 
 	return 0;
 }
 
 int
-instruction_count() {
+inst_count() {
 
-	FILE *read_instructions;
+	FILE *read_inst;
 	char c;
-	int number_of_instructions = 0;
+	int num_of_inst = 0;
 
-        read_instructions = fopen("instructions.txt", "r");
-        if (read_instructions == NULL) {
+        read_inst = fopen("instructions.txt", "r");
+        if (read_inst == NULL) {
                 fprintf(stderr, "Error reading file\n");
                 return 1;
         }
 
-	for (c = getc(read_instructions); c != EOF; c = getc(read_instructions)) {
+	for (c = getc(read_inst); c != EOF; c = getc(read_inst)) {
 		if (c == '\n') {
-			number_of_instructions = number_of_instructions + 1;
+			num_of_inst = num_of_inst + 1;
 		}
 	}
-	printf("The number of instructions = %d\n", number_of_instructions);
+	printf("The number of instructions = %d\n", num_of_inst);
 
-	fclose(read_instructions);
+	fclose(read_inst);
 	
-	return number_of_instructions;
+	return num_of_inst;
 }
 
 int
-read_instructions(int opcode[], int destination_register[], int first_source_operand[], int second_source_operand[], int number_of_instructions) {
+read_inst(int opcode[], int dest_reg[], int src1[], int src2[], int num_of_inst) {
 
-	FILE *read_instructions;
+	FILE *read_inst;
         char str[15];
         char *pch;
         int ret;
         int i = 0;
-	int dest_reg = 0;
+	int destination_register = 0;
 	int first_source = 0;
 	int second_source = 0;
 
-	read_instructions = fopen("instructions.txt", "r");
-        if (read_instructions == NULL) {
+	read_inst = fopen("instructions.txt", "r");
+        if (read_inst == NULL) {
                 fprintf(stderr, "Error reading file\n");
                 return 1;
         }
 
-	if (number_of_instructions > MAX_INSTRUCTIONS) {
-		printf("Maximum number of instruction allowed is 16\n");
-	}
-	
 	i = 0;
-	if (number_of_instructions <= MAX_INSTRUCTIONS) {
-		while (ret = fscanf(read_instructions, "%s", str)) {
+	if (num_of_inst <= MAX_INSTRUCTIONS) {
+		while (ret = fscanf(read_inst, "%s", str)) {
 
                 	if (ret == EOF) {
                         	break;
@@ -241,20 +265,20 @@ read_instructions(int opcode[], int destination_register[], int first_source_ope
 			
 				pch = strtok (NULL, "<R,>\n");
 				if (pch != NULL) {
-					dest_reg = atoi(pch);
-					destination_register[i] = dest_reg;
+					destination_register = atoi(pch);
+					dest_reg[i] = destination_register;
 				}
 
 				pch = strtok (NULL, "<R,>\n");
 				if (pch != NULL) {
 					first_source = atoi(pch);
-					first_source_operand[i] = first_source;
+					src1[i] = first_source;
 				}
 
 				pch = strtok (NULL, "<R,>\n");
 				if(pch != NULL) {
 					second_source = atoi(pch);
-					second_source_operand[i] = second_source;
+					src2[i] = second_source;
 				}
 			}
 			i++;
@@ -269,7 +293,7 @@ read_instructions(int opcode[], int destination_register[], int first_source_ope
 
 	}
 */	
-        fclose(read_instructions);
+        fclose(read_inst);
 
         return 0;
 }
@@ -300,7 +324,7 @@ print_datamemory(int M[]) {
 }
 
 void
-performing_instructions (int R[], int M[], int opcode[], int destination_register[], int first_source_operand[], int second_source_operand[], int number_of_instructions) {
+performing_inst (int R[], int M[], int opcode[], int dest_reg[], int src1[], int src2[], int num_of_inst) {
 
 	int d = 0;
 	int s1 = 0;
@@ -308,33 +332,33 @@ performing_instructions (int R[], int M[], int opcode[], int destination_registe
 	int i = 0;
 	int ld = 0;
 
-	for (i = 0; i < number_of_instructions; i++) {
+	for (i = 0; i < num_of_inst; i++) {
 		
-		d = destination_register[i];
-		s1 = first_source_operand[i];
-		s2 = second_source_operand[i];
+		d = dest_reg[i];
+		s1 = src1[i];
+		s2 = src2[i];
 		
 		printf("INSTRUCTION: %d\n", i);
 		if (opcode[i] == 1) {
 			R[d] = R[s1] + R[s2];
-			update_register_file(R, MAX_REGISTERS);
+			update_reg(R);
 			print_registers(R);
 		} else if (opcode[i] == 2) {
 			R[d] = R[s1] - R[s2];
-			update_register_file(R, MAX_REGISTERS);
+			update_reg(R);
 			print_registers(R);
 		} else if (opcode[i] == 3) {
 			R[d] = R[s1] && R[s2];
-			update_register_file(R, MAX_REGISTERS);
+			update_reg(R);
 			print_registers(R);
 		} else if (opcode[i] == 4) {
 			R[d] = R[s1] || R[s2];
-			update_register_file(R, MAX_REGISTERS);
+			update_reg(R);
 			print_registers(R);
 		} else if (opcode[i] == 5) {
 			ld = R[s1] + R[s2];
 			R[d] = M[ld];
-			update_register_file(R, MAX_REGISTERS);
+			update_reg(R);
 			print_registers(R);
 		}
 
@@ -350,23 +374,25 @@ main () {
 	 int R[MAX_REGISTERS] = {0};
 	 int M[MAX_DATAMEMORY] = {0};
 	 int opcode[MAX_INSTRUCTIONS] = {0};
-	 int destination_register[MAX_INSTRUCTIONS] = {0};
-	 int first_source_operand[MAX_INSTRUCTIONS] = {0};
-	 int second_source_operand[MAX_INSTRUCTIONS] = {0};
-	 int number_of_instructions = 0;
+	 int dest_reg[MAX_INSTRUCTIONS] = {0};
+	 int src1[MAX_INSTRUCTIONS] = {0};
+	 int src2[MAX_INSTRUCTIONS] = {0};
+	 int num_of_inst = 0;
 	 
-	 number_of_instructions = instruction_count();
-	 if(number_of_instructions > MAX_INSTRUCTIONS) {
+	 num_of_inst = inst_count();
+	 if(num_of_inst > MAX_INSTRUCTIONS) {
 		 printf("MAXIMUM NUMBER OF INSTRUCTIONS ALLOWED IS 16\n");
 		 return 0;
 	 }
 
-	 read_register_file(R, MAX_REGISTERS);
-	 update_register_file(R, MAX_REGISTERS);
-         read_datamemory_file(M, MAX_DATAMEMORY);
-         update_datamemory_file(M, MAX_DATAMEMORY);
-	 read_instructions(opcode, destination_register, first_source_operand, second_source_operand, number_of_instructions);
-	 performing_instructions (R, M, opcode, destination_register, first_source_operand, second_source_operand, number_of_instructions);
+	 read_reg(R);
+	 update_reg(R);
+         read_dam(M);
+         update_dam(M);
+	 read_inst(opcode, dest_reg, src1, src2, num_of_inst);
+	 performing_inst(R, M, opcode, dest_reg, src1, src2, num_of_inst);
+	 print_rgf(R);
+	 print_dam(M);
 
          return 0;
 }
