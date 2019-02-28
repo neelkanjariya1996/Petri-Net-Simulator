05d552c1e59af20134f1ab6c77f141480442807b
neelkanjariya1996 <neelkanjariya1996@gmail.com>
2019-02-27 19:50:06 -0500

    Getting output in simulation.txt

--- MIPSsim.c
+++ MIPSsim.c
@@ -656,35 +656,19 @@ load () {
 
 }
 
-int
-alu (FILE *fp) {
+void
+alu () {
 
 	int reg_val = 0;
 
 	if (aib[aib_head].opcode == 1) {
 		reg_val = aib[aib_head].src1 + aib[aib_head].src2;
-		if (reg_val < 0 || reg_val > 63) {
-			fprintf(fp, "Register excedding the range 0 to 63\n");
-			return -1;
-		} 
 	} else if (aib[aib_head].opcode == 2) {
 		reg_val = aib[aib_head].src1 - aib[aib_head].src2;
-		if (reg_val < 0 || reg_val > 63) {
-			fprintf(fp, "Register excedding the range 0 to 63\n");
-			return -1;
-		} 
 	} else if (aib[aib_head].opcode == 3) {
 		reg_val = aib[aib_head].src1 & aib[aib_head].src2;
-		if (reg_val < 0 || reg_val > 63) {
-			fprintf(fp, "Register excedding the range 0 to 63\n");
-			return -1;
-		} 
 	} else if (aib[aib_head].opcode == 4) {
 		reg_val = aib[aib_head].src1 | aib[aib_head].src2;
-		if (reg_val < 0 || reg_val > 63) {
-			fprintf(fp, "Register excedding the range 0 to 63\n");
-			return -1;
-		} 
 	}
 
 	reb[reb_tail].dest_reg = aib[aib_head].dest_reg;
@@ -694,8 +678,6 @@ alu (FILE *fp) {
 	reb_tail++;
 	aib_size--;
 	reb_size++;
-
-	return 0;
 
 }
 
@@ -736,7 +718,6 @@ main () {
 	int update_dam_out = 0;
 	int read_reg_out = 0;
 	int update_reg_out = 0;
-	int alu_out = 0;
 
 	memset(&inm_q, 0, sizeof(inm_q));
 	memset(&inb_q, 0, sizeof(inb_q));
@@ -796,16 +777,6 @@ main () {
 		return 0;
 	}
 	
-	decode();
-	issue1();
-	issue2();
-	addr();
-	load();
-	alu_out = alu(fp);
-	if (alu_out == -1) {
-		return 0;
-	}
-	
 	inm_size = num_of_inst;
 
 	while (inm_size != 0 || inb_size != 0 || lib_size != 0 || adb_size != 0 || aib_size != 0 || reb_size != 0) {
@@ -851,7 +822,7 @@ main () {
 		}
 
 		if (curr_aib_size != 0) {
-			alu(fp);
+			alu();
 		}
 
 		if (curr_reb_size != 0) {
